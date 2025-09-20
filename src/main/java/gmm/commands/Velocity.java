fa35fb577131457b211d0ba60b9858ed05d79441
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2025-09-20 19:25:55 +0300

    Velocity command works for @a, and command blocks

--- src/main/java/gmm/commands/Velocity.java
+++ src/main/java/gmm/commands/Velocity.java
@@ -4,12 +4,15 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.FloatArgumentType;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.util.math.Vec3d;
+
+import java.util.Collection;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
@@ -19,28 +22,34 @@ public class Velocity {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(
                     literal("velocity")
-                        .then(argument("Vx", FloatArgumentType.floatArg())
-                                .then( argument("Vy", FloatArgumentType.floatArg())
-                                    .then(argument( "Vz", FloatArgumentType.floatArg())
+                            .then(argument("target", EntityArgumentType.players())
+                                .then(argument("Vx", FloatArgumentType.floatArg())
+                                    .then( argument("Vy", FloatArgumentType.floatArg())
+                                        .then(argument( "Vz", FloatArgumentType.floatArg())
                     .executes(context -> {
 
                 ServerCommandSource source = context.getSource();
+                        Collection<ServerPlayerEntity> players =
+                                EntityArgumentType.getPlayers(context, "target");
 
-                ServerPlayerEntity player = context.getSource().getPlayer();
+
 
                 float VelocityX = FloatArgumentType.getFloat(context,"Vx");
                 float VelocityY = FloatArgumentType.getFloat(context,"Vy");
                 float VelocityZ = FloatArgumentType.getFloat(context,"Vz");
 
-                player.addVelocity(new Vec3d(VelocityX, VelocityY, VelocityZ));
-                player.velocityModified = true;
+                        for (ServerPlayerEntity player : players) {
+                            player.addVelocity(new Vec3d(VelocityX, VelocityY, VelocityZ));
+                            player.velocityModified = true;
+                        }
 
-                source.sendFeedback(() -> Text.literal("Velocity of player changed."), true);
+
+                source.sendFeedback(() -> Text.literal("Velocity of " + players.size() +  " player(s) changed."), true);
                 return Command.SINGLE_SUCCESS;
                     })
                                     )
                                 )
                         )
-            );
+            ));
     }
     }
